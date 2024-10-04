58bd50b9a3ce3d7c2fbe4f1aead99204daffbe8b
maria <maria@HPmv0905>
2024-10-04 20:18:09 +0200

    Initial commit

---
+++ MyHayDay/src/myhayday/MyHayDay.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myhayday;
+
+/**
+ *
+ * @author maria
+ */
+public class MyHayDay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
