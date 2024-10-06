9affadb07968dde4b633d95cdebdb95984305846
maria <maria@HPmv0905>
2024-10-06 12:21:02 +0200

    finished 1st part

--- MyHayDay/src/modelos/Trigo.java
+++ MyHayDay/src/modelos/Trigo.java
@@ -24,4 +24,70 @@ public class Trigo {
     private int precioVenta;
     private int riego;
     
+
+    //--------------------------
+    // Constructores
+    // -------------------------
+    
+    
+    public Trigo(String icono, int duracionProduccion, int experienciaRecolecta, int precioVenta, int riego){
+        this.icono = icono;
+        this.duracionProduccion = duracionProduccion;
+        this.experienciaRecolecta = experienciaRecolecta;
+        this.precioVenta = precioVenta;
+        this.riego = riego; 
+    }
+
+    public Trigo() {
+        this(ICONO, DURACION,EXPERIENCIA,PRECIO_VENTA, RIEGO);
+    }
+    
+     public Trigo(Trigo otro){
+        this(otro.ICONO, otro.DURACION,otro.EXPERIENCIA,otro.PRECIO_VENTA, otro.RIEGO);
+        
+    }
+
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+    
+    public int getduracionProduccion(){
+        return duracionProduccion;
+    }
+    
+    public void setduracionProduccion(int duracionProduccion){
+        this.duracionProduccion = duracionProduccion;
+    }
+    
+    public int getexperienciaRecolecta(){
+        return experienciaRecolecta;
+    }
+    
+    public void setexperienciaRecoecta (int experienciaRecolecta){
+        this.experienciaRecolecta = experienciaRecolecta;
+    }
+    
+    public int getprecioVenta(){
+        return precioVenta;
+    }
+    
+    public void setprecioVenta(int precioVenta){
+        this.precioVenta = precioVenta;
+    }
+    
+    public int getriego(){
+        return riego;
+    }
+    
+    public void setriego(int riego){
+        this.riego = riego;
+    }
+    
+     
+    
+    
 }
--- MyHayDay/src/myhayday/MyHayDay.java
+++ MyHayDay/src/myhayday/MyHayDay.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package myhayday;
+import modelos.Trigo;
 
 /**
  *
@@ -15,6 +16,34 @@ public class MyHayDay {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Trigo trigo= new Trigo("HOLA", 10, 5, 2, 1);
+        
+        System.out.println(trigo.getduracionProduccion());
+        System.out.println(trigo.getIcono());
+        System.out.println(trigo.getexperienciaRecolecta());
+        System.out.println(trigo.getprecioVenta());
+        System.out.println(trigo.getriego());
+        
+        Trigo trigo1 = new Trigo();
+        
+        Trigo trigo2 = new Trigo(trigo);
+        
+        System.out.println(trigo1.getduracionProduccion());
+        System.out.println(trigo1.getIcono());
+        System.out.println(trigo1.getexperienciaRecolecta());
+        System.out.println(trigo1.getprecioVenta());
+        System.out.println(trigo1.getriego());
+        
+        
+        System.out.println(trigo2.getduracionProduccion());
+        System.out.println(trigo2.getIcono());
+        System.out.println(trigo2.getexperienciaRecolecta());
+        System.out.println(trigo2.getprecioVenta());
+        System.out.println(trigo2.getriego());
+        
+        
+        
     }
     
 }
